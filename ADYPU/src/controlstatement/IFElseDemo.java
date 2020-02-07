deb968932f26f78f4e2241d628a9543beb3c4904
admin <admin@DESKTOP-6FQJOCG>
2020-02-07 08:10:21 -0800

    if else, else if, loops, switch case completed.

--- ADYPU/src/controlstatement/IFElseDemo.java
+++ ADYPU/src/controlstatement/IFElseDemo.java
@@ -14,7 +14,26 @@ public class IFElseDemo {
 
 }
    
+    void checkElseIf(int number){
 
+        if (number<35){
+            System.out.printf("Fail");
+        }
+        else if(number>=35 && number<60){
+            System.out.printf("Pass Class");
+        }
+
+        else if (number>=60 && number<74){
+            System.out.printf("First Class");
+        }
+        else if (number>=74 && number<=100){
+            System.out.printf("Dist");
+        }
+        else {
+            System.out.printf("Invalid");
+        }
+
+    }
 
     public static void main(String[] args) {
         IFElseDemo demo=new IFElseDemo();
@@ -22,7 +41,7 @@ public class IFElseDemo {
         System.out.print("Enter number : ");
         int number=scanner.nextInt();
         //demo.checkIfElse(number);
-        demo.checkIfElse(number);
+        demo.checkElseIf(number);
     }
 
 }
